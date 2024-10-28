b4dee126333877ea0470f01dfca32ddc484b6560
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-28 07:24:39 +0200

    Start studying PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo"I love burgers! <br>";
+echo"Also pizza";
+// simple comment
+/* 
+multiline comment
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order burgers</button>
+</body>
+</html>
