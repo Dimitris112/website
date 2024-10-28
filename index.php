1d7547e5aeedcc72370b04248ce4c7ae0e21ceb0
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-28 08:49:50 +0200

    GET - POST

--- index.php
+++ index.php
@@ -1,44 +1,64 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get vs Post</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+    <label>username:</label><br>
+    <input type="text" name="username"><br>
+
+    <label">password:</label><br>
+    <input type="password" name="password"><br>
+
+    <input type="submit" value="Log in">
+    </form>
+</body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <label for="quantity">quantity:</label><br>
+        <input type="text" name="quantity"><br>
+        <input type="submit" value="total">
+    </form>
+</body>
+</html>
+
+
 <?php
-$x = 10;
-$y = 3;
-$z = null;
+// echo "{$_GET['username']} <br>";
+// echo "{$_GET['password']} <br>";
 
-// boolean in PHP -> true = 1 / false = empty
-
-// $z = $x + $y;
-// $z = $x - $y;
-// $z = $x * $y;
-// $z = $x / $y;
-// $z = $x % $y;
-// $z = $x ** $y;
-// $z = $x == $y;
-// $z = $x === $y;
-// $z = $x != $y;
-// $z = $x <> $y;
-// $z = $x < $y;
-// $z = $x > $y;
-// $z = $x <= $y;
-// $z = $x >= $y;
-// echo $z;
-
-
-// $counter = 0;
-// // $counter++;
-// // $counter--;
-// $counter+=3;
-
-// echo $counter;
+// echo "{$_POST['username']} <br>";
+// echo "{$_POST['password']} <br>";
 
 
 /*
-()
-**
-* / %
-+ -
+$_GET = Data is appended to the url - not secure - char limit - bookmark possible w/ values - GET requests can be cached - better for search page
+
+$_POST = Data is packaged inside the body of the HTTP request - more secure - no data limit - cannot bookmark - GET requests are not cached - better for submitting credentials
+
 */
-$total = 1 + 2 - 3 * 4 / 5 ** 6;
 
-echo $total;
 
+$item = "pizza";
+$price = 5.50;
+$quantity = $_POST["quantity"];
+$total = null;
+
+$total = $quantity * $price;
+
+echo"Ordered {$quantity} {$item}/s for \${$total}";
 
 ?>
