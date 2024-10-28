384a945622c74d99ab1b7dee33b930c59236b8fd
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-28 09:21:13 +0200

    Some math functions

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Get vs Post</title>
-</head>
-<body>
-    <form action="index.php" method="POST">
-    <label>username:</label><br>
-    <input type="text" name="username"><br>
-
-    <label">password:</label><br>
-    <input type="password" name="password"><br>
-
-    <input type="submit" value="Log in">
-    </form>
-</body>
-</html> -->
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,38 +6,60 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="POST">
-        <label for="quantity">quantity:</label><br>
-        <input type="text" name="quantity"><br>
+    <!-- <form action="index.php" method="POST">
+        <label for="x">x:</label>
+        <input type="text" name="x"><br>
+        <label for="y">y:</label>
+        <input type="text" name="y"><br>
+        <label for="z">z:</label>
+        <input type="text" name="z"><br>
         <input type="submit" value="total">
-    </form>
+    </form> -->
+
+    <form action="index.php" method="post">
+        <label>radius:</label>
+        <input type="text" name="radius"><br>
+        <input type="submit" value="Calculate">
+    </form><br>
+
 </body>
 </html>
-
-
 <?php
-// echo "{$_GET['username']} <br>";
-// echo "{$_GET['password']} <br>";
+// $x = $_POST["x"];
+// $y = $_POST["y"];
+// $z = $_POST["z"];
+// $total = null;
 
-// echo "{$_POST['username']} <br>";
-// echo "{$_POST['password']} <br>";
+// $total = abs($x);
+// $total = round($x);
+// $total = floor($x);
+// $total = ceil($x);
+// $total = sqrt($x);
+// $total = pow($x, $y);
+// $total = max($x, $y, $z);
+// $total = min($x, $y, $z);
+// $total = pi();
+// $total = rand(1, 6); // like a dice
+// echo $total;
 
 
-/*
-$_GET = Data is appended to the url - not secure - char limit - bookmark possible w/ values - GET requests can be cached - better for search page
 
-$_POST = Data is packaged inside the body of the HTTP request - more secure - no data limit - cannot bookmark - GET requests are not cached - better for submitting credentials
+$radius = $_POST['radius'];
+$circumference = null;
+$area = null;
+$volume = null;
 
-*/
 
+$circumference = 2 * pi() * $radius;
+$circumference = round($circumference, 2); // round to 2 decimal
 
-$item = "pizza";
-$price = 5.50;
-$quantity = $_POST["quantity"];
-$total = null;
+$area = pi() * pow($radius, 2);
+$area = round($area, 2);
 
-$total = $quantity * $price;
+$volune = 4 / 3 * pi() * pow($radius, 3);
+$volune = round($volune, 2);
 
-echo"Ordered {$quantity} {$item}/s for \${$total}";
-
+echo"Circumference = {$circumference}cm <br>";
+echo"Area = {$area}cm<sup>2</sup> <br>";
+echo"Volume = {$volune}cm<sup>3</sup>";
 ?>
