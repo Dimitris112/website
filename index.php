bc780c310a536886c13deda0278e0d7488623654
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-28 07:56:43 +0200

    Simple arithmetic operators

--- index.php
+++ index.php
@@ -1,32 +1,44 @@
 <?php
-$name = "Dimitris";
-$food = "pizza";
-$email = "dimitris@me.com";
+$x = 10;
+$y = 3;
+$z = null;
 
-$age = 28;
-$price = 4.50;
-$quantity = 3;
+// boolean in PHP -> true = 1 / false = empty
 
-$online = true;
-$employed = false;
-// true = 1  - false = nothing / empty
-
-$total = null;
-
-echo "Hey {$name}!<br>";
-echo" I like {$food}!<br>";
-echo" My email is {$email}!<br>";
-
-echo" I am {$age} years old!<br>";
-echo" Price for a pizza slice is \${$price}!<br>";
-
-echo" Online status: {$online}<br>";
-echo" Employed status: {$employed}<br>";
+// $z = $x + $y;
+// $z = $x - $y;
+// $z = $x * $y;
+// $z = $x / $y;
+// $z = $x % $y;
+// $z = $x ** $y;
+// $z = $x == $y;
+// $z = $x === $y;
+// $z = $x != $y;
+// $z = $x <> $y;
+// $z = $x < $y;
+// $z = $x > $y;
+// $z = $x <= $y;
+// $z = $x >= $y;
+// echo $z;
 
 
+// $counter = 0;
+// // $counter++;
+// // $counter--;
+// $counter+=3;
 
-echo" Ordered {$quantity} slices of {$food}";
-$total = $price * $quantity;
-echo" for a total of \${$total}!<br>";
+// echo $counter;
+
+
+/*
+()
+**
+* / %
++ -
+*/
+$total = 1 + 2 - 3 * 4 / 5 ** 6;
+
+echo $total;
+
 
 ?>
