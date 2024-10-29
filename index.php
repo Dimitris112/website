a7d7eb181e0bcb61b6d70aa6f10ef1d7c61e1bc4
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-29 20:13:37 +0200

    Simple for loop

--- index.php
+++ index.php
@@ -1,47 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="POST">
+        <!-- <label for="number">Enter a number to count to:</label><br> -->
+        <label for="number">Enter a number to count down from:</label><br>
+        <input type="number" name="number"><br>
+        <input type="submit" value="Start">
+    </form>
+</body>
+
+</html>
+
+
 <?php
-
-$grade = "B";
-$date = date("l");
-
-// $date = "asdad";
-
-// switch ($grade) {
-//     case "A":
-//         echo "Excellent";
-//         break;
-//     case "B":
-//         echo "Very Good";
-//         break;
-//     case "C":
-//         echo "Good";
-//         break;
-//     default:
-//         echo "{$grade} is not a valid grade";
+// for($i =2;$i <=30;$i+=2){
+//     echo $i . "\n";
 // }
 
-switch($date){
-    case "Monday":
-        echo "Happy Monday";
-        break;
-    case "Tuesday":
-        echo "Happy Tuesday";
-        break;
-    case "Wednesday":
-        echo "Happy Wednesday";
-        break;
-    case "Thursday":
-        echo "Happy Thursday";
-        break;
-    case "Friday":
-        echo "Happy Friday";
-        break;
-    case "Saturday":
-        echo "Happy Saturday";
-        break;
-    case "Sunday":
-        echo "Happy Sunday";
-        break;
-    default:
-        echo "{$date} is not a valid date";
+// for($i = 10; $i > 0; $i--){
+//     echo $i . "\n";
+// }
+
+
+
+$counter = $_POST["counter"];
+// // for ($i = 1; $i <= $counter; $i++) {
+// //     echo $i . "<br>";
+// // }
+
+for ($i = $counter; $i > 0; $i--) {
+    echo $i . "<br>";
 }
+
 ?>
