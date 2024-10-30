41e78f764ad02bf22c73e6d4fb1dcae1bad4bd36
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-30 15:45:59 +0200

    Simple include function

--- index.php
+++ index.php
@@ -1,47 +1,26 @@
+<?php
+// include() - copies the contents of a file into the current PHP script / sections of the website become reusable - changes only need to be made in 1 place
+// similar to 'base.html' - {% %} block-endblock tags / extends used in Django --- export default / import components - functions in React
+include("header.html");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 
 <body>
-    <form action="index.php" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" name="username"><br>
-        <label for="email">Email:</label><br>
-        <input type="email" name="email"><br>
-        <label for="age">Age:</label><br>
-        <input type="text" name="age"><br>
-        <label for="height">Height:</label><br>
-        <input type="text" name="height"><br>
-        <input type="submit" name="login" value="login">
-    </form>
+    This is the Home page <br>
+    Stuff goes here
 </body>
 
 </html>
 
 <?php
-// sanitize & validate input - filter_input() - FILTER_SANITIZE_SPECIAL_CHARS - FILTER_SANITIZE_NUMBER_INT - FILTER_SANITIZE_NUMBER_FLOAT - FILTER_SANITIZE_EMAIL -- FILTER_VALIDATE_INT - FILTER_VALIDATE_FLOAT - FILTER_VALIDATE_EMAIL
-
-// if (isset($_POST['login'])) {
-//     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
-//     $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
-//     $height = filter_input(INPUT_POST, 'height', FILTER_VALIDATE_FLOAT);
-//     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-//     echo "Welcome {$username}";
-//     echo "<br>";
-//     echo "Age: {$age}";
-//     echo "<br>";
-//     echo "Height: {$height}";
-//     echo "<br>";
-//     echo "Email: {$_POST['email']}";
-// }
-
-
-if (isset($_POST['login'])) {
-    $age = filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT);
-}
+include("footer.html");
 ?>
